c3709e9c7176607e161976aff38db81ef2961583
averyboy <averyboy@qq.com>
2019-03-13 15:48:22 +0800

    update threadpool

--- src/os/线程池/threadpool.cpp
+++ src/os/线程池/threadpool.cpp
@@ -1,7 +1,45 @@
-//reference:https://www.zhihu.com/search?type=content&q=linux%E7%BA%BF%E7%A8%8B%E6%B1%A0%E8%AE%BE%E8%AE%A1
 #include "threadpool.h"
-// #include<bits/stdc++.h>
+#include<pthread.h>
+#include<cstdio>
+#include<cstdlib>
 ThreadPool* threadpool_init(int num_thread,int queue_max_job_num)
 {
-    if(thread_num  <=0 || queue_max_job_num<=0)
+    if(num_thread<=0 || queue_max_job_num<=0)
+    {
+        perror("invalid args!");
+        return NULL;
+    }
+    ThreadPool *pool=(ThreadPool*)malloc(sizeof(ThreadPool));
+    if(NULL==pool)
+    {
+        perror("cannot to malloc threadpool!");
+        return NULL;
+    }
+    pool->num_thread=num_thread;
+    pool->queue_max_job_num=queue_max_job_num;
+    pool->queue_cur_num_job=0;
+    pool->head=NULL;
+    pool->tail=NULL;
+    if(pthread_mutex_init(&(pool->mutex),NULL))
+    {
+        perror("failed to init mutex!");
+        free(pool);
+        pool=NULL;
+        return NULL;
+    }
+    if(pthread_cond_init(&(pool->queue_empty),NULL))
+    {
+        perror("failed to init queue_empty!");
+        free(pool);
+        pool=NULL;
+        return NULL;
+    }
+    if(pthread_cond_init(&(pool->queue_not_empty),NULL))
+    {
+        perror("failed to init queue_not_empty!");
+        free(pool);
+        pool=NULL;
+        return NULL;
+    }
+    if(pthread_cond_init(&()))
 }
